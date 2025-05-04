9af96341d37f58b8b401711a16888bcb41724315
Yolanda <yeckdemies@gmail.com>
2025-05-04 16:09:42 +0200

    Gestión de imagen Mejorada

--- src/helpers/uploadToCloudinary.js
+++ src/helpers/uploadToCloudinary.js
@@ -10,7 +10,9 @@ cloudinary.config({
 const uploadToCloudinary = (buffer) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
-      { folder: 'vehiculos' },
+      { folder: 'vehiculos',
+        resource_type: 'raw'
+       },
       (error, result) => {
         if (error) return reject(error);
         resolve(result.secure_url);
